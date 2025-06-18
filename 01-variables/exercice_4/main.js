8da9a0916be7c7097cc8b4720f9044a0090032d7
kombozipremice <premicek18@gmail.com>
2025-06-18 14:26:10 +0100

    Resolution des exercices

--- 01-variables/exercice_4/main.js
+++ 01-variables/exercice_4/main.js
@@ -1,3 +1,5 @@
+let a = 5;
+let b = 8;
 
-
-
+let somme = a + b;
+console.log(somme);
--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -1,6 +1,18 @@
 let paul, marie, eric, clair, madameMukuna, joseph, sarah;
 // Ecrivez votre code ici
+let maisonFamiliale = 60000000;
+let terrains = 40000000;
+let liquidites = 20000000;
+let heritage = maisonFamiliale + terrains + liquidites;
 
+paul = (heritage * 0.75) / 3;
+marie = (heritage * 0.75) / 3;
+eric = (heritage * 0.75) / 3 / 2;
+clair = (heritage * 0.75) / 3 / 2;
+
+madameMukuna = (heritage * 0.25) / 3;
+joseph = (heritage * 0.25) / 3;
+sarah = (heritage * 0.25) / 3;
 // Ne pas modifier le code ci-dessous
 module.exports = {
   paul,
